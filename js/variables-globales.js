289ffa6cb5e7a177c09efdb809dc954e17b2263f
alex traverso <alextraverso6@gmail.com>
2022-12-19 10:54:49 -0300

    create remote repo

---
+++ js/variables-globales.js
@@ -0,0 +1,42 @@
+var modoRelax = false;
+var movimientos = 0;
+var cronometro;
+var grupoTarjetas = [
+  ["⚽", "🏀"],
+  ["🏈", "🏐"],
+  ["🐶", "🐱", "🐺"],
+  ["🍌", "🍓", "🍉"],
+  ["🚕", "🚙", "🚗"],
+];
+var nivelActual = 0;
+var niveles = [
+  {
+    tarjetas: grupoTarjetas[0],
+    movimientosMax: 3,
+  },
+  {
+    tarjetas: grupoTarjetas[0].concat(grupoTarjetas[1]),
+    movimientosMax: 8,
+  },
+  {
+    tarjetas: grupoTarjetas[0].concat(grupoTarjetas[1], grupoTarjetas[2]),
+    movimientosMax: 12,
+  },
+  {
+    tarjetas: grupoTarjetas[0].concat(
+      grupoTarjetas[1],
+      grupoTarjetas[2],
+      grupoTarjetas[3]
+    ),
+    movimientosMax: 25,
+  },
+  {
+    tarjetas: grupoTarjetas[0].concat(
+      grupoTarjetas[1],
+      grupoTarjetas[2],
+      grupoTarjetas[3],
+      grupoTarjetas[4]
+    ),
+    movimientosMax: 60,
+  },
+];
